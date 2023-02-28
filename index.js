ef3c93431c27f16ffc5d965570c5b34dab94671b
ValeriaMtzg <126584501+ValeriaMtzg@users.noreply.github.com>
2023-02-28 08:00:40 -0600

    Examen P1 Mobil

---
+++ index.js
@@ -0,0 +1,46 @@
+/* Realice cuatro (de tres tipos) 
+function declaration, expression, arrow de suma, resta, division y multiplicacion
+con valores por default, muestra solo en console y cuales son las diferencias entre ellas */
+
+// Diferencias
+
+
+
+
+function sumar(a,b) {
+    return   resultado ='Tu valor es :' (a + b);
+  }
+  
+function restar (a,b){
+
+    return resultado ='Tu valor es : ' (a-b);
+}
+
+function division(a,b){
+
+    return resultado ='Tu valor es : ' (a/b);
+}
+
+function multiplica(a, b){
+
+    return resultado 'Tu valor es: ' (a*b);
+}
+
+
+console.log(sumar(25,66));
+console.log(restar(22,55));
+console.log(division(4,2));
+console.log(multiplica(6,6));
+
+
+/*function arreglo(valores){
+    console.log('Arreglo  '+valores)
+
+    for(let i=0; i <=10, i++) {
+
+        console.log( valor +'x'+i + '='+ valor*i)
+    }
+} 
+
+*/
+
